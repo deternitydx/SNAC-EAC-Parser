76e3ed99e942ab63b2c8c66685aa7a1e23255249
Robbie Hott <jh2jf@virginia.edu>
2014-12-10 10:52:48 -0500

    EDM Model
    Switched to Europeana, which can handle most of the data we're trying to
    define about each person.  Also, allowed for the adding of places and
    birth and death dates to the output.

--- generate_triples.py
+++ generate_triples.py
@@ -1,4 +1,5 @@
 import codecs
+import os
 # Import the bulbs graph ORM
 from bulbs.neo4jserver import Graph
 from relationships import *
@@ -13,6 +14,7 @@ output.write("@prefix foaf: <http://xmlns.com/foaf/0.1/> .\n")
 output.write("@prefix owl: <http://www.w3.org/2002/07/owl#> .\n")
 output.write("@prefix schema: <http://schema.org/> .\n")
 output.write("@prefix edm: <http://www.europeana.eu/schemas/edm/> .\n")
+output.write("@prefix skos: <http://www.w3.org/2004/02/skos/core#> .\n")
 output.write("@prefix rdaGr2: <http://RDVocab.info/ElementsGr2/> .\n")
 
 
@@ -40,17 +42,18 @@ g.add_proxy("referencedIn", ReferencedIn)
 namespaces = { "snac" : "urn:isbn:1-931666-33-4" ,
         "snac2" : "http://socialarchive.iath.virginia.edu/control/term#",
         "schema" : "http://schema.org/",
-        "xlink" : "http://www.w3.org/1999/xlink"}
+        "xlink" : "http://www.w3.org/1999/xlink",
+        "snac3" : "http://socialarchive.iath.virginia.edu/"}
 ET.register_namespace("snac", "urn:isbn:1-931666-33-4")
 ET.register_namespace("snac2", "http://socialarchive.iath.virginia.edu/control/term#")
+ET.register_namespace("snac3", "http://socialarchive.iath.virginia.edu/")
 ET.register_namespace("xlink", "http://www.w3.org/1999/xlink")
 
-files = [ 'sample_eac.xml', 'sample_eac2.xml' ]
 # For each file, parse and look at
+path = "sample/"
+for filename in os.listdir(path):
 
-for filename in files:
-
-    tree = ET.parse(filename)
+    tree = ET.parse(os.path.join(path,filename))
     root = tree.getroot()
     #for child in root:
     #    print child.tag, child.attrib
@@ -69,20 +72,33 @@ for filename in files:
     corresponded = []
     sameas = []
     entity_type = ""
+    start = None
+    end = None
     #need birth/death/active dates
 
     # Handle identifier
     node = root.find(".//snac:recordId", namespaces)
     identifier = node.text
 
+    # Handle birth/death/active dates
+    node = root.find(".//snac:existDates", namespaces)
+    if node is not None:
+        tmp = node.find(".//snac:fromDate", namespaces)
+        if tmp is not None:
+            start = tmp.get("standardDate")
+        tmp = node.find(".//snac:toDate", namespaces)
+        if tmp is not None:
+            end = tmp.get("standardDate")
+
     # Handle entity type
     node = root.find(".//snac:entityType", namespaces)
     if node.text == "person":
-        entity_type = "schema:Person"
+        entity_type = "edm:Agent"
     elif node.text == "corporateBody":
-        entity_type = "schema:Organization"
+        entity_type = "edm:Agent"
     else:
-        entity_type = "schema:Thing"
+        entity_type = "edm:Agent"
+    hidden_type = node.text
 
     # Handle names
     first = True;
@@ -102,8 +118,8 @@ for filename in files:
                 nationalities.append(node[0].text)
         
     # Handle places
-    for node in root.findall(".//snac:place", namespaces):
-        print node[0].text
+    for node in root.findall(".//snac3:placeEntryLikelySame", namespaces):
+        places.append(node.get("vocabularySource"))
         # this will be tricky:
         #   if LikelySameAs exists, then use it and also grab coordinates and geonames entry for owl:sameAs
         #   else, then just grab the text that exists (node[0].text)
@@ -147,31 +163,38 @@ for filename in files:
 
     # Write out the triples
     output.write(''.join(["<",identifier,"> a <", entity_type, "> .\n"]))
-    output.write(''.join(["<",identifier,"> schema:name \"", name, "\" .\n"]))
+    output.write(''.join(["<",identifier,"> skos:prefLabel \"", name, "\" .\n"]))
+    if start is not None:
+        output.write(''.join(["<",identifier,"> edm:start \"", start, "\" .\n"]))
+    if end is not None:
+        output.write(''.join(["<",identifier,"> edm:end \"", end, "\" .\n"]))
+    
+    if hidden_type == 'person':
+        output.write(''.join(["<",identifier,"> foaf:name \"", name, "\" .\n"]))
     for altname in alt_names:
         if altname is not None:
-            output.write(''.join(["<",identifier,"> schema:alternateName \"", altname, "\" .\n"]))
+            output.write(''.join(["<",identifier,"> skos:altLabel \"", altname, "\" .\n"]))
     for subject in subjects:
         if subject is not None:
-            output.write(''.join(["<",identifier,"> snac:hasSubject \"", subject, "\" .\n"]))
+            output.write(''.join(["<",identifier,"> edm:isRelatedTo \"", subject, "\" .\n"]))
     for nationality in nationalities:
         if nationality is not None:
             output.write(''.join(["<",identifier,"> schema:nationality \"", nationality, "\" .\n"]))
     for language in languages:
         if language is not None:
-            output.write(''.join(["<",identifier,"> snac:hasLanguage \"", language, "\" .\n"]))
+            output.write(''.join(["<",identifier,"> rdaGr2:languageOfThePerson \"", language, "\" .\n"]))
     for place in places:
         if place is not None:
-            output.write(''.join(["<",identifier,"> snac:hasPlace \"", place, "\" .\n"]))
+            output.write(''.join(["<",identifier,"> edm:hasMet <", place, "> .\n"]))
     for occupation in occupations:
         if occupation is not None:
-            output.write(''.join(["<",identifier,"> snac:hasOccupation \"", occupation, "\" .\n"]))
+            output.write(''.join(["<",identifier,"> rdaGr2:professionOrOccupation \"", occupation, "\" .\n"]))
     for assn in associated:
         if assn is not None:
-            output.write(''.join(["<",identifier,"> snac:associatedWith <", assn , "> .\n"]))
+            output.write(''.join(["<",identifier,"> edm:hasMet <", assn , "> .\n"]))
     for corr in corresponded:
         if corr is not None:
-            output.write(''.join(["<",identifier,"> snac:correspondedWith <", corr, "> .\n"]))
+            output.write(''.join(["<",identifier,"> edm:hasMet <", corr, "> .\n"]))
     for same in sameas:
         if same is not None:
             output.write(''.join(["<",identifier,"> owl:sameAs <", same, "> .\n"]))
